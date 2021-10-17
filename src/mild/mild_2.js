6f90b61ca4c48f5c8062ba8d9c8056e7f7e33377
zzz-tang <zziyun@email.unc.edu>
2021-10-17 19:59:59 -0400

    Finished removeKey() for mild_2.js.

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -51,7 +51,7 @@ export function identifyArray(array) {
  obj now does not contain the `password` field
  */
 export function removeKey(object, key) {
-
+  delete object[key];
 }
 
 /**
--- test.js
+++ test.js
@@ -35,3 +35,16 @@ import { identifyArray } from "./src/mild/mild_2.js";
 let unknownElementsArray = [ 'some', 3, [3, 4], false ];
 console.log( "\nTest identifyArray()" );
 console.log( identifyArray(unknownElementsArray) );
+
+// Test removeKey()
+import { removeKey } from "./src/mild/mild_2.js";
+let obj = {
+  name: 'Mr. Boss',
+  title: 'boss',
+  age: 33,
+  password: 'pass123'
+};
+console.log( "\nTest removeKey()" );
+console.log(obj);
+removeKey(obj, 'password');
+console.log(obj);
