809d460eb3543edf2c2ac8d5f358e3c732050c66
zzz-tang <zziyun@email.unc.edu>
2021-10-17 17:51:00 -0400

    Finished sumToString() for mild_1.js.

--- src/mild/mild_1.js
+++ src/mild/mild_1.js
@@ -9,7 +9,7 @@
  * see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
  */
 export function sumToString(a, b) {
-
+  return `${a} + ${b} = ${a + b}`;
 }
 
 
---
+++ test.js
@@ -0,0 +1,2 @@
+import {sumToString} from "./src/mild/mild_1.js";
+console.log(sumToString(1,2));
